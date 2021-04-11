eb26f765a4a86cc6200d8f0a4c76765268f1b9ea
AlvinRuan <53242017+AlvinRuan@users.noreply.github.com>
2021-04-10 22:53:28 -0400

    started heaps

---
+++ Practice Problems/Heaps.js
@@ -0,0 +1,11 @@
+//What is a binary Heap?
+
+// Very similar to a Binary Search Tree, but some different rules.
+
+// In a MaxBinaryHeap, parent nodes are alawys larger than child nodes.
+
+//          no order is necessary
+
+// In MinBinaryHeap, parent nodes are always smaller than child nodes.
+
+// In binary heaps, it's as compacted as possible. Not those super long 1 leg trees.
